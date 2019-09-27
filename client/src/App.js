104f41ac8ce5e9c734ca92c0a7ad17a298cf7eaf
ilikeblink182too <chris.aaron.williams@gmail.com>
2019-09-27 13:21:39 -0500

    footer and navbar exist

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/layouts/Navbar';
+import Footer from './components/layouts/Footer';
+
 
 function App() {
   return (
+    
     <div className="App">
+      <Navbar />
     <h1>Root Page</h1>
+    <Footer />
     </div>
   );
 }
