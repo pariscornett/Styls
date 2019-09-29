b5f904e85c26ac3049f3c8beee6bf7a1a59084af
Paris Cornett <pariscornett@gmail.com>
2019-09-28 20:52:53 -0500

    fixed mongodb connection error

---
+++ config/passport.js
@@ -0,0 +1,18 @@
+// const JwtStrategy = require("passport-jwt").Strategy;
+// const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+// //const db = require("../models");
+// //const keys = require("../keys");
+
+// const options = {};
+// options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// options.secretOrKey = keys.secretOrKey;
+
+// module.exports= passport => {
+
+//     passport.use(
+//         new JwtStrategy(options, (jwtPayload, done) => {
+             
+//         })
+//     )
+// }
