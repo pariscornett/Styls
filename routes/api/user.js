02442d8deb030c7431462709b96da9efc7999c65
Paris Cornett <pariscornett@gmail.com>
2019-09-30 16:18:05 -0500

    removed unnecessary code from routes/api/user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -13,47 +13,6 @@ module.exports = function (app) {
     });
 
     // @route POST /api/user
-    // @desc creates a new user
-    // app.post("/api/user", (req, res) => {  
-    //     db.User.find({                      //looks for existing user by querying where email = email user inputs
-    //         email: req.body.email
-    //     })
-    //     .then(User => {
-    //         if(User) {                      //if we have a record of the email provided, throw an error
-    //             return res.status(400).json({ 
-    //                 email: "This email already exists"
-    //             });
-    //         } else {                        //if we do not have a record of the email provided, create a new User (this mirrors our UserSchema)
-    //             const newUser = {
-    //                 firstName: req.body.firstName,
-    //                 lastName: req.body.lastName,
-    //                 email: req.body.email,
-    //                 password: req.body.password,
-    //                 date: Date.now
-    //             }
-
-    //             //hashes password before storage in db
-    //             bcrypt.genSalt(10, (err, salt) => {
-    //                 bcrypt.hash(newUser.password, salt, (err, hash) => {
-    //                     if(err) throw(err);                             //if there is an error, throw the error
-    //                     newUser.password = hash;                          // otherwise, reassign the password to the hash
-
-    //                     //now, we are able to save this to db.
-    //                     User.create(newUser)
-    //                         .then(User => {
-    //                             res.status(200).json({
-    //                                 msg: newUser.firstName + "'s account was successfully created!",
-    //                                 userCreated: true
-    //                             });
-    //                         })
-    //                         .catch(err => console.log(err));
-    //                 })
-    //             })
-    //         }
-    //     })
-    // }) 
-
-    // @route POST /api/user
     // @desc creates a new user (references class activity 18 in the Mongo/Mongoose folder)
     app.post("/api/user", function (req, res){
         User.findOne({
