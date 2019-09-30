94e15d1d25472889ca505931255578909bc4f2eb
Micaela Bray <micaela.bray@gmail.com>
2019-09-30 12:54:44 -0500

    Update CreateCloset.js

--- client/src/components/pages/CreateCloset.js
+++ client/src/components/pages/CreateCloset.js
@@ -1,1 +1,12 @@
 //Add items to your closet/create new closet
+import React from 'react';
+
+function CreateCloset(props) {
+    return (
+        <div>
+            <h1>Add Item</h1>
+        </div>
+    )
+}
+
+export default CreateCloset
