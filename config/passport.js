7753b724b6c894cf10c86108ee5aa33422631c65
Paris Cornett <pariscornett@gmail.com>
2019-09-30 19:43:29 -0500

    changed passport.js to reflect proper capitalization

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 
-const db = require("../models/User");
+const User = require("../models/User");
 const keys = require("../config/keys");
 
 const options = {};
@@ -17,8 +17,8 @@ module.exports= passport => {
                 id: jwtPayload.SECRET_OR_KEY
              })
              .then(User => {
-                if(user) {
-                    return done(null, user); //runs if successful
+                if(User) {
+                    return done(null, User); //runs if successful
                 }
                 return done(null, false); //runs if unsuccessful
              })
